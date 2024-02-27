a088ed7325e4843b0d063509a0c425e2d22b6cf5
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-26 22:12:46 -0300

    Add files via upload

---
+++ Meus Dados/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Meus Dados
+//
+//  Created by Usuário Convidado on 22/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+   
+    @IBOutlet weak var lbNome: UILabel!
+    
+    @IBOutlet weak var lbCidade: UILabel!
+    
+    @IBOutlet weak var lbIdade: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lbNome.text="Seu nome é..."
+        lbCidade.text="Sua cidade é...."
+        lbIdade.text = "Sua idade é...."
+    }
+
+    @IBAction func Exibir(_ sender: Any) {
+        lbNome.text="Agesandro Scarpioni"
+        lbCidade.text="São Paulo"
+        lbIdade.text = "20 anos"
+    }
+    
+    @IBAction func Limpar(_ sender: Any) {
+        lbNome.text="Seu nome é..."
+        lbCidade.text="Sua cidade é...."
+        lbIdade.text = "Sua idade é...."
+    }
+}
+
